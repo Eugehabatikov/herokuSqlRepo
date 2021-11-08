9278fcc3fc61ad98b837eb1bd05752e79495dff9
евгений <79500420830@yandex.ru>
2021-11-09 01:07:05 +0300

    replase all code, because have problem in master branch.  Have 2 working endpoint: /hello /hell

--- src/main/java/ru.simple/dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package ru.simple.dao;
-
-import ru.simple.pojo.User;
-
-import java.util.List;
-
-public interface UserMapper {
-    public List<User> findUserByName(String s);
-}
--- src/main/java/ru.simple/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package ru.simple.service;
-import ru.simple.pojo.User;
-
-import java.util.List;
-
-public interface UserService {
-    public User findUserById(int id) throws Exception;
-    public List<User> findUserByName(String userName) throws Exception;
-}
